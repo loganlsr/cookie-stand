bfa1f6625d5a6e8c1226f8c5b45fbd8803382425
Logan Rogers <logan.s.rogers@biola.edu>
2016-06-20 13:46:05 -0700

    more java written

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-'use strict';
 
 var pike = {
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   min: 23,
   max: 65,
   avg: 6.3,
-  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
 };
 
 pike.listHours = function () {
-  var pikeInfo = document.getElementById(pikeInfo);
+  var pikeInfo = document.getElementById('pikeInfo');
+  for (var i = 0; i < pike.hours.length; i++) {
+    console.log(pike.hours[i]);
+    var listItem = document.createElement('li');
+    listItem.textContent = pike.hours[i];
+    pikeInfo.appendChild(listItem);
+  };
 };
+
+pike.listHours();
