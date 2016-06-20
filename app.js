ff6e1212e18c542170c4ad850de3f36889669b4f
Logan Rogers <logan.s.rogers@biola.edu>
2016-06-20 13:19:08 -0700

    started writing functions

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var pike = {
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+};
+
+pike.listHours = function () {
+  var pikeInfo = document.getElementById(pikeInfo);
+};
